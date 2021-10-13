9b870590efc83e952865d41847c4dd02aa51ee0c
ufuokt <39736702+ufuokt@users.noreply.github.com>
2021-10-14 00:00:54 +0300

    Create create_databases.sql

---
+++ AzureDatabricksWorkspace/setup/create_databases.sql
@@ -0,0 +1,8 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS db_processed
+LOCATION "/mnt/dataenggpprjdl/processed"
+
+-- COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS db_presentation
+LOCATION "/mnt/dataenggpprjdl/presentation"
